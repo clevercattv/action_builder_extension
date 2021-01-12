0c17d628c595e0da9c8170bfaf5a5d0cefda0920
Maksym Vitkovskyi <clevercattv@gmail.com>
2021-01-12 20:34:05 +0200

    Added operation storage

---
+++ js/content/storage.js
@@ -0,0 +1,52 @@
+const storage = (() => {
+    const operationKey = 'operations';
+
+    /**
+     * @return {Promise<<Operation>[]>}
+     */
+    const getOperations = () => new Promise(resolve => {
+        chrome.storage.local.get(operationKey,
+            result => resolve(result[operationKey] ? result[operationKey] : []))
+    });
+
+    /**
+     * @return {Promise<<Operation>[]>}
+     */
+    const getCurrentSiteOperation = async () => {
+        const operations = await getOperations();
+        const href = window.location.href;
+        return Array.from(operations).filter(operation =>
+            operation.regExes.some(regEx => href.match(regEx)));
+    }
+
+    /**
+     * @param {Operation} operation
+     * @return {Promise<void>}
+     */
+    const createOperation = async operation => {
+        let operations = await getOperations();
+        operations = operations ? operations : [];
+        operations.push(operation);
+        chrome.storage.local.set({operations});
+    }
+
+    /**
+     * @param {Operation} operation
+     * @return {Promise<void>}
+     */
+    const removeOperation = async operation => {
+        const operations = await getOperations();
+        const filtered = operations.filter(opr => opr.title === operation.title)
+            .filter(opr => opr.regExes.every(oprRegEx =>
+                operation.regExes.some(regEx => regEx === oprRegEx)));
+        operations.splice(operations.indexOf(filtered.pop()), 1);
+        chrome.storage.local.set({operations});
+    }
+
+    return {
+        getOperations,
+        getCurrentSiteOperation,
+        createOperation,
+        removeOperation
+    }
+})()
