307d73e7df9410f10572a1a76fa1c9f59ff44bcc
Maksym Vitkovskyi <clevercattv@gmail.com>
2021-01-20 23:34:13 +0200

    Added popupV2 (almost finished)

--- js/view/popup_page_view.js
+++ js/view/popup_page_view.js
@@ -1,4 +1,19 @@
+/*
+checked-action #70AC85; No errors, all needed fields filled.
+warning-action #FFDF8E; Not all needed fields filled.
+error-action   #FD8B7B; Get's some errors while try to action.
+ */
 window.addEventListener('load', async () => {
+    // enable bootstrap tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip({
+            delay: {
+                show: 500,
+                hide: 200
+            }
+        });
+    });
+
     const displayNone = 'd-none';
     const container = document.querySelector('[class*=container]');
     const operations = await storage.getOperations();
@@ -13,6 +28,68 @@ window.addEventListener('load', async () => {
         currentPageOperations: [],
         otherPagesOperations: [],
     });
+
+    const buildOperation = async operation => {
+        const operationElement = await fetchFirstBodyElement('html/popup/popup_operation.html');
+        operationElement.querySelector('#title').innerText = operation.title;
+
+        operationElement.querySelector('#launch').innerText = 'Launch type: ' + operation.launch.type
+            + (operation.launch.keys ? ` [${operation.launch.keys.reduce((prev, cur) => `${prev} + ${cur}`)}]` : '');
+
+        operationElement.querySelector('#actionTitle').innerText = `Actions [${operation.actions.length}]`;
+
+        operationElement.querySelector('#edit')
+            .addEventListener('click', () => chrome.tabs.create({
+                url: `${location.protocol}//${location.hostname}/html/edit_operation.html?operation=${operation.id}`
+            }));
+
+        operationElement.querySelector('#remove')
+            .addEventListener('click', () =>
+                storage.removeOperation(operation).then(() => operationElement.remove()));
+
+        const isEnabledInput = operationElement.querySelector('#isEnabled');
+        isEnabledInput.checked = operation.isEnabled;
+        isEnabledInput.addEventListener('click', () =>
+            storage.updateOperationIsEnabled(operation.isEnabled = isEnabledInput.checked, operation.id));
+
+        const actionsContainer = operationElement.querySelector('#actions');
+        for (const [index, action] of operation.actions.entries()) {
+            actionsContainer.appendChild(await buildAction(action, index));
+        }
+
+        return operationElement;
+    }
+
+    const buildAction = async (action, index) => {
+        const actionElement = await fetchFirstBodyElement('html/popup/popup_action.html');
+        const nameElement = actionElement.querySelector('#name');
+        nameElement.innerText = `[${index + 1}] ${action.typeName}`;
+        if (action.error) {
+            nameElement.parentElement.classList.add('error-action');
+        } else if (!action.selector || (action.selector && action.selector !== '')) {
+            nameElement.parentElement.classList.add('checked-action');
+        } else {
+            nameElement.parentElement.classList.add('warning-action');
+        }
+        // todo add edit selector icon button
+
+        return actionElement;
+    }
+
+    container.querySelector('#options').addEventListener('click', () => chrome.tabs.create({
+        url: `${location.protocol}//${location.hostname}/html/options.html`
+    }));
+
+    const currentOperationsElement = container.querySelector('#currentOperations');
+    const otherOperationsElement = container.querySelector('#otherOperations');
+    for (const operation of groupedOperations.currentPageOperations) {
+        currentOperationsElement.appendChild(await buildOperation(operation));
+    }
+    for (const operation of groupedOperations.otherPagesOperations) {
+        otherOperationsElement.appendChild(await buildOperation(operation));
+    }
+    container.querySelector('#currentOperationsTitle').innerText = `Current site actions [${groupedOperations.currentPageOperations.length}]`;
+    container.querySelector('#otherOperationsTitle').innerText = `Other site actions [${groupedOperations.otherPagesOperations.length}]`;
 
     const getCardElements = cardElement => {
         return {
@@ -96,23 +173,23 @@ window.addEventListener('load', async () => {
         headingElement.innerText = text;
         return headingElement;
     };
-
-    if (groupedOperations.currentPageOperations.length) {
-        container.appendChild(buildH3CenteredElement('Current page actions'));
-
-        for (const operation of groupedOperations.currentPageOperations) {
-            await buildOperationCard(operation);
-        }
-    }
-
-    if (groupedOperations.otherPagesOperations.length) {
-        container.appendChild(buildH3CenteredElement('Other pages actions'));
-
-        for (const operation of groupedOperations.otherPagesOperations) {
-            const cardElement = await buildOperationCard(operation);
-            const launchOperationButton = cardElement.querySelector('#launchOperation');
-            launchOperationButton.parentElement.removeChild(launchOperationButton);
-        }
-    }
+    //
+    // if (groupedOperations.currentPageOperations.length) {
+    //     container.appendChild(buildH3CenteredElement('Current page actions'));
+    //
+    //     for (const operation of groupedOperations.currentPageOperations) {
+    //         await buildOperationCard(operation);
+    //     }
+    // }
+    //
+    // if (groupedOperations.otherPagesOperations.length) {
+    //     container.appendChild(buildH3CenteredElement('Other pages actions'));
+    //
+    //     for (const operation of groupedOperations.otherPagesOperations) {
+    //         const cardElement = await buildOperationCard(operation);
+    //         const launchOperationButton = cardElement.querySelector('#launchOperation');
+    //         launchOperationButton.parentElement.removeChild(launchOperationButton);
+    //     }
+    // }
 
 })
